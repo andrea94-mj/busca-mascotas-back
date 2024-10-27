d4fc9c130e4736e2e5212452b51ee7a50e2a057d
Andrea Miguel Jarillo <andreamj94@gmail.com>
2024-10-27 23:08:03 +0100

    commit

--- back/index.js
+++ back/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import mongoRoutes from './routes/mongodb.routes';
+// import mongoRoutes from './routes/mongodb.routes';
 
 const router = express();
 
 router.use(express.json());
 router.use(express.urlencoded({extended:true}));
 
-router.use('/api/v1/mongo', mongoRoutes);
+// router.use('/api/v1/', mongoRoutes);
 
 router.listen(3000, ()=>{
     console.log("Servidor 3000 corriendo")
