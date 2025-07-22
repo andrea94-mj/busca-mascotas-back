ad2c430858903afaf98f7cfa9b402ce07a8bdf36
Andrea Miguel Jarillo <andreamj94@gmail.com>
2025-07-22 19:29:43 +0200

    commit

--- back/index.js
+++ back/index.js
@@ -19,7 +19,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Habilitar CORS para permitir peticiones 
 // app.use(cors());
 app.use(cors({
-    origin: 'https://proyecto-final-fullstack-front-gsze.vercel.app'
+    origin: [
+      'http://localhost:5173', // desarrollo Vite
+      'http://localhost:3000', // desarrollo adicional
+      'https://proyecto-final-fullstack-front-gsze.vercel.app' // producción
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
 // Habilitar JSON en las solicitudes
